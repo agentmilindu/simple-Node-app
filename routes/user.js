58025c605f7b5937730dbe905b5e94f79e96752d
agentmilindu <agentmilindu@gmail.com>
2013-11-10 11:36:51 +0530

    Initial

---
+++ routes/user.js
@@ -0,0 +1,51 @@
+
+/*
+ * GET localhost/users : users listing.
+ */
+
+exports.list = function(req, res){
+  res.send("respond with a resource");
+};
+
+/*
+ * GET localhost/users/login : users login form.
+ */
+exports.login = function(req, res){
+  res.render('login', {   title: 'Login!', message : req.session.msg  });
+};
+
+/*
+ * GET localhost/users/dash : users dashboard.
+ */
+exports.dash = function(req, res){
+  if (req.session.user) { 
+        res.render('dash', {   title: 'Dash', username: req.session.user.username });
+    }
+    //
+    else{
+        req.session.msg = "Please login first"
+        res.redirect("/users/login")
+    }
+};
+
+/*
+ * POST localhost/users/logger : users logger.
+ */
+exports.logger = function (req, res) {
+    if (req.body.name=="admin" && req.body.password=="123") { 
+        req.session.user = { username: req.body.name }
+        res.redirect("/users/dash")
+    }
+    //
+    else{
+        req.session.msg = "username password missmatch"
+        res.redirect("/users/login")
+    } 
+};
+
+/*
+ * GET localhost/users/:username : users profile.
+ */
+exports.profile = function(req, res){
+  res.render('profile', {   title: 'Profile', username: req.params.username });
+};
---
+++ server.js
@@ -0,0 +1,43 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({secret: 'ytghhgiikjlkjghjjhh'}));
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+//Routs
+
+app.get('/', routes.index); // GET localhost/
+app.get('/users', user.list); // GET localhost/users
+app.get('/users/dash', user.dash); // GET localhost/dash
+app.get('/users/login', user.login); // GET localhost/login
+app.post('/users/logger', user.logger); // POST localhost/users/logger
+app.get('/users/:username', user.profile); // GET  localhost/users/bill
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
